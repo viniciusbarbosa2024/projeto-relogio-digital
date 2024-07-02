01218b3c737c1a4014bf980c7571d37068b101f4
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-02 13:39:42 -0300

    Terminei o javascript e aprimorei o style

--- js/script.js
+++ js/script.js
@@ -7,9 +7,36 @@ const unidadeSegundos = document.getElementsByClassName('relogio')[5]
 
 function relogio() {
     let segundos = new Date().getSeconds()
+    let minutos = new Date().getMinutes()
+    let horas = new Date().getHours()
+
+    if (segundos < 10) {
+        dezenaSegundos.innerHTML = 0
+        unidadeSegundos.innerHTML = String(segundos).charAt(0)    
+    } else {
+       dezenaSegundos.innerHTML = String(segundos).charAt(0) //Anotar sobre o charAt()
+       unidadeSegundos.innerHTML = String(segundos).charAt(1) 
+    }
     
-    dezenaSegundos.innerHTML = Math.trunc(segundos/10)
+    if (minutos < 10) {
+        dezenaMinutos.innerHTML = 0    
+        unidadeMinutos.innerHTML = String(minutos).charAt(0)
+    } else {
+        dezenaMinutos.innerHTML = String(minutos).charAt(0)
+        unidadeMinutos.innerHTML = String(minutos).charAt(1)
+    }
+
+    if(horas < 10) {
+        dezenaHoras.innerHTML = 0
+        unidadeHoras.innerHTML = String(horas).charAt(0)
+    } else {
+        dezenaHoras.innerHTML = String(horas).charAt(0)
+        unidadeHoras.innerHTML = String(horas).charAt(1)
+    }
+
+    
 }
 
-relogio()
+setInterval(relogio,1000) //Anotar sobre setInterval
 
+//relogio()
